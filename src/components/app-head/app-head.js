094bd422dadd3c653918645027b2ecca495aba31
SHOX <theshoxjaxon@gmail.com>
2022-05-21 01:35:10 +0500

    added new components for built site

---
+++ src/components/app-head/app-head.js
@@ -0,0 +1,16 @@
+import "./app-head";
+function AppHeaders() {
+    return (
+        <div>
+        <div className="info-mode d-flex align-items-center">
+            <div class="fa-solid fa-phone">
+                <a href="tel:+8 (812) 123-45-67">8 (812) 123-45-67</a>
+            </div>
+                <p >8 (812) 123-45-67    |    Работаем 7 дней в неделю    |    9:00 — 18:00  </p>
+            <div className="align-items-center">
+          </div>     
+          </div>
+        </div>
+    )
+}
+export default  AppHeaders;
---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import AppHeaders from "../app-head/app-head";
+
+
+import "./app.css"; 
+
+
+function App (){
+    return (
+        <div className="container p-1">
+            <AppHeaders />        
+        </div>
+    )
+}
+
+export default App
